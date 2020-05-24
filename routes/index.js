d6ece6b9d68bdee11048df8dac56397a3b97f0d9
AsaExit <asaj71@gmail.com>
2020-05-24 21:12:07 +0200

    Added delete, put and patch to the routerfiles

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,8 @@ const tank = require('./tank.js')
 router.post("/tanks", tank.post)
 router.get("/tanks", tank.get)
 router.get("/tanks/:id", tank.getById)
+router.delete("/tanks/:id", tank.deleteById)
+router.put("/tanks/:id", tank.put)
+router.patch("/tanks/:id", tank.patch)
 
 module.exports = router
